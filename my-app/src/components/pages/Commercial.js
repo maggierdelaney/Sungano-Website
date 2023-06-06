d7fab14b2316e2952b3cd4352c5d1f6d69a4bf78
maggierdelaney <111933814+maggierdelaney@users.noreply.github.com>
2023-06-05 22:12:34 -0500

    trialed another color scheme, changed to more attractive font, uploaded commerical and editorial photos into bootstrap cards

--- my-app/src/components/pages/Commercial.js
+++ my-app/src/components/pages/Commercial.js
@@ -3,8 +3,43 @@ import './style.css';
 
 export default function Commercial() {
   return (
-    <div class="container">
-      <h2>Commercial</h2>
+    <div id="com-cards">
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/black-crop.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+        </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/blue-dress-side.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/blue-dress.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/romper-side.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/romper.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/white-shirt-front.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/white-shirt-up.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+    
     </div>
   );
 }
--- my-app/src/components/pages/Editorial.js
+++ my-app/src/components/pages/Editorial.js
@@ -3,8 +3,122 @@ import './style.css';
 
 export default function Editorial() {
   return (
-    <div class="container">
-      <h2>Editorial</h2>
+    <div id="edi-cards">
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/black-front.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+        </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/black-lunge.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/black-neck.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/black-side.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/black-squat.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/color-neck.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/braids.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/hat.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/jacket-body.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/jacket-front.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/joshua-tree.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/overalls-floor.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/overalls-front.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/pattern-far.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/pattern-front.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/pattern-lean.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/pattern-lunge.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/rock-far.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/rock-front.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/rock-side.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/shoes.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/white-jacket-front.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/editorial/white-jacket-up.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
     </div>
   );
 }
