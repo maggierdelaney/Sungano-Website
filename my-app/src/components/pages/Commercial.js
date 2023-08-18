3cc7204a5df4942799fdb0489f286ea90752d15e
maggierdelaney <111933814+maggierdelaney@users.noreply.github.com>
2023-08-17 19:20:09 -0500

    added new commercial photos

--- my-app/src/components/pages/Commercial.js
+++ my-app/src/components/pages/Commercial.js
@@ -5,12 +5,52 @@ export default function Commercial() {
   return (
     <div id="com-cards">
       <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/makeup-close.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/makeup-lip.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/makeup-right.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/makeup-skin.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/denim-lounge.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/denim-stand.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/denim-stool.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
         <img class="card-img-top" src={require("../images/commercial/romper-side.JPG")} alt="Card image cap"></img>
         {/* <div class="card-body">
           </div> */}
       </div>
       <div class="card">
         <img class="card-img-top" src={require("../images/commercial/romper.JPG")} alt="Card image cap"></img>
+        {/* <div class="card-body">
+          </div> */}
+      </div>
+      <div class="card">
+        <img class="card-img-top" src={require("../images/commercial/romper-sun.JPG")} alt="Card image cap"></img>
         {/* <div class="card-body">
           </div> */}
       </div>
