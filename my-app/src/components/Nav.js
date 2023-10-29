57e09e14f270975df0e1c07a2f6eb639accc3aba
maggierdelaney <111933814+maggierdelaney@users.noreply.github.com>
2023-10-29 15:06:12 -0700

    fixed nav bar lettering to original bold

--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -8,19 +8,19 @@ function Nav({ currentPage, handlePageChange }) {
         <li className="nav-item">
           <a className="nav-link active" href="#home"
             onClick={() => handlePageChange('Home')}
-            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}><strong>H</strong>ome</a>
+            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>Home</a>
         </li>
         <li className="nav-item">
           <a className="nav-link active" href="#commercial" onClick={() => handlePageChange('Commercial')}
-            className={currentPage === 'Commercial' ? 'nav-link active' : 'nav-link'}><strong>c</strong>ommercial</a>
+            className={currentPage === 'Commercial' ? 'nav-link active' : 'nav-link'}>Commercial</a>
         </li>
         <li className="nav-item">
           <a className="nav-link active" href="#editorial" onClick={() => handlePageChange('Editorial')}
-            className={currentPage === 'Editorial' ? 'nav-link active' : 'nav-link'}><strong>e</strong>ditorial</a>
+            className={currentPage === 'Editorial' ? 'nav-link active' : 'nav-link'}>Editorial</a>
         </li>
         <li className="nav-item">
           <a className="nav-link active" href="#contact" onClick={() => handlePageChange('Contact')}
-            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}><strong>c</strong>ontact</a>
+            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a>
         </li>
 
         <li className="nav-item">
