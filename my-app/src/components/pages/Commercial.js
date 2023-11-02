42da53d377ff07b4cbddf529eee2f51f5bafb76b
maggierdelaney <111933814+maggierdelaney@users.noreply.github.com>
2023-11-01 19:02:05 -0500

    making images clickable and open into the same window in a modal as opposed to a new window

--- my-app/src/components/pages/Commercial.js
+++ my-app/src/components/pages/Commercial.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 import './style.css';
 
 /*makeup photos top row */
@@ -24,98 +25,241 @@ import sweaterFace from '../images/commercial/sweater-face.JPG';
 import sweaterLegs from '../images/commercial/sweater-legs.JPG';
 
 export default function Commercial() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openModal = (imageSrc) => {
+    setSelectedImage(imageSrc);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+    setShowModal(false);
+  };
+
   return (
     <div>
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <a href={makeupCloseImage} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={makeupCloseImage} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={makeupLipImage} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={makeupLipImage} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={makeupRightImage} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={makeupRightImage} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={makeupSkinImage} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={makeupSkinImage} alt="Card image cap"></img>
-            </a>
-          </div>
+      <div className="container">
+        <div className="row">
+          {[
+            makeupCloseImage,
+            makeupLipImage,
+            makeupRightImage,
+            makeupSkinImage,
+          ].map((imageSrc, index) => (
+            <div className="col" key={index}>
+              <img
+                className="card-img-top"
+                src={imageSrc}
+                alt="Card image cap"
+                onClick={() => openModal(imageSrc)}
+              />
+            </div>
+          ))}
         </div>
       </div>
-      <br></br>
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <a href={denimLounge} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={denimLounge} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={denimStand} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={denimStand} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={denimStool} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={denimStool} alt="Card image cap"></img>
-            </a>
-          </div>
+<br></br>
+      <div className="container">
+        <div className="row">
+          {[
+            denimLounge,
+            denimStand,
+            denimStool,
+          ].map((imageSrc, index) => (
+            <div className="col" key={index}>
+              <img
+                className="card-img-top"
+                src={imageSrc}
+                alt="Card image cap"
+                onClick={() => openModal(imageSrc)}
+              />
+            </div>
+          ))}
         </div>
       </div>
-      <br></br>
-      <div class="container">
-        <div class="row">
-        <div class="col">
-            <a href={romperSide} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={romperSide} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={romper} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={romper} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={romperSun} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={romperSun} alt="Card image cap"></img>
-            </a>
-          </div>
+<br></br>
+      <div className="container">
+        <div className="row">
+          {[
+            romperSide,
+            romper,
+            romperSun,
+          ].map((imageSrc, index) => (
+            <div className="col" key={index}>
+              <img
+                className="card-img-top"
+                src={imageSrc}
+                alt="Card image cap"
+                onClick={() => openModal(imageSrc)}
+              />
+            </div>
+          ))}
         </div>
       </div>
-      <br></br>
-      <div class="container">
-        <div class="row">
-        <div class="col">
-            <a href={sweaterBody} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={sweaterBody} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={sweaterChin} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={sweaterChin} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={sweaterFace} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={sweaterFace} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={sweaterLegs} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={sweaterLegs} alt="Card image cap"></img>
-            </a>
-          </div>
+<br></br>
+      <div className="container">
+        <div className="row">
+          {[
+            sweaterBody,
+            sweaterChin,
+            sweaterFace,
+            sweaterLegs,
+          ].map((imageSrc, index) => (
+            <div className="col" key={index}>
+              <img
+                className="card-img-top"
+                src={imageSrc}
+                alt="Card image cap"
+                onClick={() => openModal(imageSrc)}
+              />
+            </div>
+          ))}
         </div>
       </div>
-      <br></br>
+<br></br>
+      <Modal show={showModal} onHide={closeModal} centered>
+        <Modal.Body>
+          {selectedImage && (
+            <img
+              src={selectedImage}
+              alt="Selected Image"
+              style={{ width: '100%' }}
+            />
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
+
+
+
+
+//ORIGINAL
+// import React from 'react';
+// import './style.css';
+
+// /*makeup photos top row */
+// import makeupCloseImage from '../images/commercial/makeup-close.JPG';
+// import makeupLipImage from '../images/commercial/makeup-lip.JPG';
+// import makeupRightImage from '../images/commercial/makeup-right.JPG';
+// import makeupSkinImage from '../images/commercial/makeup-skin.JPG';
+
+// /*denim romper photos */
+// import denimLounge from '../images/commercial/denim-lounge.JPG';
+// import denimStand from '../images/commercial/denim-stand.JPG';
+// import denimStool from '../images/commercial/denim-stool.JPG';
+
+// /*romper outdoors photos */
+// import romperSide from '../images/commercial/romper-side.JPG';
+// import romper from '../images/commercial/romper.JPG';
+// import romperSun from '../images/commercial/romper-sun.JPG';
+
+// /*black and white sweater photos */
+// import sweaterBody from '../images/commercial/sweater-body.JPG';
+// import sweaterChin from '../images/commercial/sweater-chin.JPG';
+// import sweaterFace from '../images/commercial/sweater-face.JPG';
+// import sweaterLegs from '../images/commercial/sweater-legs.JPG';
+
+// export default function Commercial() {
+//   return (
+//     <div>
+//       <div class="container">
+//         <div class="row">
+//           <div class="col">
+//             <a href={makeupCloseImage} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={makeupCloseImage} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={makeupLipImage} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={makeupLipImage} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={makeupRightImage} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={makeupRightImage} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={makeupSkinImage} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={makeupSkinImage} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//       <br></br>
+//       <div class="container">
+//         <div class="row">
+//           <div class="col">
+//             <a href={denimLounge} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={denimLounge} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={denimStand} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={denimStand} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={denimStool} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={denimStool} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//       <br></br>
+//       <div class="container">
+//         <div class="row">
+//         <div class="col">
+//             <a href={romperSide} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={romperSide} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={romper} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={romper} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={romperSun} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={romperSun} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//       <br></br>
+//       <div class="container">
+//         <div class="row">
+//         <div class="col">
+//             <a href={sweaterBody} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={sweaterBody} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={sweaterChin} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={sweaterChin} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={sweaterFace} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={sweaterFace} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={sweaterLegs} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={sweaterLegs} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//       <br></br>
+//     </div>
+//   );
+// }
--- my-app/src/components/pages/Editorial.js
+++ my-app/src/components/pages/Editorial.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 import './style.css';
 
 /*braids pictures */
@@ -28,120 +29,289 @@ import whiteJacketUp from '../images/editorial/white-jacket-up.JPG';
 import JoshuaTreeEdit from '../images/editorial/joshua-tree-edit.JPG';
 import rockSide from '../images/editorial/rock-side.JPG';
 
+export default function Commercial() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
-export default function Editorial() {
+  const openModal = (imageSrc) => {
+    setSelectedImage(imageSrc);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+    setShowModal(false);
+  };
+
   return (
     <div>
       <div className="container">
-        <div className="row align-items-center">
-          <div class="col">
-            <a href={jacketBody} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={jacketBody} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={braids} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={braids} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={jacketFront} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={jacketFront} alt="Card image cap"></img>
-            </a>
-          </div>
+        <div className="row">
+          {[
+            jacketBody,
+            braids,
+            jacketFront,
+          ].map((imageSrc, index) => (
+            <div className="col" key={index}>
+              <img
+                className="card-img-top"
+                src={imageSrc}
+                alt="Card image cap"
+                onClick={() => openModal(imageSrc)}
+              />
+            </div>
+          ))}
         </div>
       </div>
       <br></br>
       <div className="container">
-        <div className="row align-items-center">
-          <div class="col">
-            <a href={patternLean} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={patternLean} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={patternFront} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={patternFront} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={patternFar} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={patternFar} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={patternLunge} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={patternLunge} alt="Card image cap"></img>
-            </a>
-          </div>
+        <div className="row">
+          {[
+            patternLean,
+            patternFront,
+            patternFar,
+            patternLunge,
+          ].map((imageSrc, index) => (
+            <div className="col" key={index}>
+              <img
+                className="card-img-top"
+                src={imageSrc}
+                alt="Card image cap"
+                onClick={() => openModal(imageSrc)}
+              />
+            </div>
+          ))}
         </div>
       </div>
       <br></br>
       <div className="container">
-        <div className="row align-items-center">
-          <div class="col">
-            <a href={blackLunge} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={blackLunge} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={blackFront} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={blackFront} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={blackSide} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={blackSide} alt="Card image cap"></img>
-            </a>
-          </div>
+        <div className="row">
+          {[
+            blackLunge,
+            blackFront,
+            blackSide,
+          ].map((imageSrc, index) => (
+            <div className="col" key={index}>
+              <img
+                className="card-img-top"
+                src={imageSrc}
+                alt="Card image cap"
+                onClick={() => openModal(imageSrc)}
+              />
+            </div>
+          ))}
         </div>
       </div>
       <br></br>
       <div className="container">
-        <div className="row align-items-center">
-          <div class="col">
-            <a href={overallsFloor} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={overallsFloor} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={overallsFront} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={overallsFront} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={shoes} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={shoes} alt="Card image cap"></img>
-            </a>
-          </div>
+        <div className="row">
+          {[
+            overallsFront,
+            overallsFloor,
+            shoes,
+          ].map((imageSrc, index) => (
+            <div className="col" key={index}>
+              <img
+                className="card-img-top"
+                src={imageSrc}
+                alt="Card image cap"
+                onClick={() => openModal(imageSrc)}
+              />
+            </div>
+          ))}
         </div>
       </div>
       <br></br>
       <div className="container">
-        <div className="row align-items-center">
-          <div class="col">
-            <a href={sandBag} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={sandBag} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={whiteJacketUp} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={whiteJacketUp} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={JoshuaTreeEdit} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={JoshuaTreeEdit} alt="Card image cap"></img>
-            </a>
-          </div>
-          <div class="col">
-            <a href={rockSide} target="_blank" rel="noopener noreferrer">
-              <img class="card-img-top" src={rockSide} alt="Card image cap"></img>
-            </a>
-          </div>
+        <div className="row">
+          {[
+            sandBag,
+            whiteJacketUp,
+            JoshuaTreeEdit,
+            rockSide
+          ].map((imageSrc, index) => (
+            <div className="col" key={index}>
+              <img
+                className="card-img-top"
+                src={imageSrc}
+                alt="Card image cap"
+                onClick={() => openModal(imageSrc)}
+              />
+            </div>
+          ))}
         </div>
       </div>
       <br></br>
+      <Modal show={showModal} onHide={closeModal} centered>
+        <Modal.Body>
+          {selectedImage && (
+            <img
+              src={selectedImage}
+              alt="Selected Image"
+              style={{ width: '100%' }}
+            />
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
-  )
+  );
 }
+
+
+
+
+
+
+
+// import React from 'react';
+// import './style.css';
+
+// /*braids pictures */
+// import jacketBody from '../images/editorial/jacket-body.JPG';
+// import braids from '../images/editorial/braids.JPG';
+// import jacketFront from '../images/editorial/jacket-front.JPG';
+
+// /*pattern dress images */
+// import patternLean from '../images/editorial/pattern-lean.JPG';
+// import patternFront from '../images/editorial/pattern-front.JPG';
+// import patternFar from '../images/editorial/pattern-far.JPG';
+// import patternLunge from '../images/editorial/pattern-lunge.JPG';
+
+// /*black and white images */
+// import blackLunge from '../images/editorial/black-lunge.JPG';
+// import blackFront from '../images/editorial/black-front.JPG';
+// import blackSide from '../images/editorial/black-side.JPG';
+
+// /*green overalls images */
+// import overallsFloor from '../images/editorial/overalls-floor.JPG';
+// import overallsFront from '../images/editorial/overalls-front.JPG';
+// import shoes from '../images/editorial/shoes.JPG';
+
+// /*last row joshua tree desert images */
+// import sandBag from '../images/editorial/sand-bag.JPG';
+// import whiteJacketUp from '../images/editorial/white-jacket-up.JPG';
+// import JoshuaTreeEdit from '../images/editorial/joshua-tree-edit.JPG';
+// import rockSide from '../images/editorial/rock-side.JPG';
+
+
+// export default function Editorial() {
+//   return (
+//     <div>
+//       <div className="container">
+//         <div className="row align-items-center">
+//           <div class="col">
+//             <a href={jacketBody} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={jacketBody} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={braids} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={braids} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={jacketFront} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={jacketFront} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//       <br></br>
+//       <div className="container">
+//         <div className="row align-items-center">
+//           <div class="col">
+//             <a href={patternLean} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={patternLean} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={patternFront} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={patternFront} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={patternFar} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={patternFar} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={patternLunge} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={patternLunge} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//       <br></br>
+//       <div className="container">
+//         <div className="row align-items-center">
+//           <div class="col">
+//             <a href={blackLunge} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={blackLunge} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={blackFront} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={blackFront} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={blackSide} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={blackSide} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//       <br></br>
+//       <div className="container">
+//         <div className="row align-items-center">
+//           <div class="col">
+//             <a href={overallsFloor} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={overallsFloor} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={overallsFront} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={overallsFront} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={shoes} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={shoes} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//       <br></br>
+//       <div className="container">
+//         <div className="row align-items-center">
+//           <div class="col">
+//             <a href={sandBag} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={sandBag} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={whiteJacketUp} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={whiteJacketUp} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={JoshuaTreeEdit} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={JoshuaTreeEdit} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//           <div class="col">
+//             <a href={rockSide} target="_blank" rel="noopener noreferrer">
+//               <img class="card-img-top" src={rockSide} alt="Card image cap"></img>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//       <br></br>
+//     </div>
+//   )
+// }
